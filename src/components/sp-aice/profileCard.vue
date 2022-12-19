0102ced53e9052afd94c1cae3f05b5db933e4e5d
axelhagerback <axel.hagerback@gmail.com>
2022-12-19 15:29:09 +0100

    url title, and made profile cards to component

---
+++ src/components/sp-aice/profileCard.vue
@@ -0,0 +1,50 @@
+<script>
+export default {
+  data() {
+    return {
+      toggled: false,
+    };
+  },
+  methods: {
+    flipCard() {
+      this.toggled = !this.toggled;
+    },
+  },
+  props: {},
+};
+</script>
+
+<template>
+  <div class="w-[300px] h-[300px] mx-auto" @click="flipCard">
+    <div
+      class="innerCard cursor-pointer w-[100%] h-[100%] transition transform duration-1000 relative"
+      :class="toggled ? 'isFlipped' : ''"
+    >
+      <div class="cardface bg-red-700">Axel</div>
+      <div class="cardface cardfaceBack">Inte Axel</div>
+    </div>
+  </div>
+</template>
+
+<style>
+.innerCard {
+  transform-style: preserve-3d;
+}
+
+.innerCard.isFlipped {
+  transform: rotateY(180deg);
+}
+
+.cardface {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  backface-visibility: hidden;
+  -webkit-backface-visibility: hidden;
+}
+
+.cardfaceBack {
+  background-color: blue;
+  transform: rotateY(180deg);
+}
+</style>
--- src/views/SpaiceView.vue
+++ src/views/SpaiceView.vue
@@ -1,77 +1,21 @@
 <script setup>
 import AppHeader from "../components/AppHeader.vue";
 import AppSectionTitle from "../components/AppSectionTitle.vue";
+import profileCard from "../components/sp-aice/profileCard.vue";
 import { RouterLink } from "vue-router";
 </script>
 
-<script>
-export default {
-  data() {
-    return {
-      toggled: false,
-    };
-  },
-  methods: {
-    flipCard() {
-      this.toggled = !this.toggled;
-    },
-  },
-};
-</script>
 <template>
   <AppHeader class="flex-grow-0 flex-shrink-0"
     ><RouterLink to="/sp-aice">SP-AICE</RouterLink></AppHeader
   >
   <main class="flex-grow">
     <AppSectionTitle name="Team" />
-
-    <div class="card">
-      <div
-        class="innerCard cursor-pointer"
-        :class="toggled ? 'isFlipped' : ''"
-        @click="flipCard"
-      >
-        <div class="cardface cardfaceFront">Axel</div>
-        <div class="cardface cardfaceBack">Inte Axel</div>
-      </div>
+    <div class="grid grid-cols-2 w-[50%] mx-auto">
+      <profileCard />
+      <profileCard />
+      <profileCard />
+      <profileCard />
     </div>
   </main>
 </template>
-
-<style>
-.card {
-  margin: 100px auto 0;
-  width: 400px;
-  height: 600px;
-}
-
-.innerCard {
-  width: 100%;
-  height: 100%;
-  transition: transform 1s;
-  transform-style: preserve-3d;
-  cursor: pointer;
-  position: relative;
-}
-
-.innerCard.isFlipped {
-  transform: rotateY(180deg);
-}
-
-.cardface {
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  backface-visibility: hidden;
-  -webkit-backface-visibility: hidden;
-}
-
-.cardfaceFront {
-  background-color: red;
-}
-
-.cardfaceBack {
-  background-color: blue;
-  transform: rotateY(180deg);
-}
-</style>
