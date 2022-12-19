e767f73752dfaff0cae0d8fc72eaca8bc65ac6c9
Olivoz <lucas.sivars@gmail.com>
2022-12-19 09:23:14 +0100

    Setup Header and Footer

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@ import { RouterView } from "vue-router";
 </script>
 
 <template>
-  <main>
+  <header class="flex-grow-0 flex-shrink-0">Header</header>
+  <main class="flex-grow">
     <RouterView />
   </main>
+  <footer class="flex-grow-0 flex-shrink-0">Footer</footer>
 </template>
