3ac8ace551366c82871eff1a9daee81f7e28c4e1
elliotabrahamsson <elliotsson@outlook.com>
2023-01-11 13:28:11 +0100

    added window for porjects of Sp-AICE

--- src/views/SpaiceView.vue
+++ src/views/SpaiceView.vue
@@ -7,15 +7,18 @@ import isacPicture from "../assets/isac.svg";
 import elliotPicture from "../assets/elliot.svg";
 import casperPicture from "../assets/casper.svg";
 import { RouterLink } from "vue-router";
+import ProjectCard from "../components/wood-ridge/ProjectCard.vue";
 </script>
 
 <template>
   <AppHeader class="flex-grow-0 flex-shrink-0"
     ><RouterLink to="/sp-aice">SP-AICE</RouterLink></AppHeader
   >
-  <main >
+  <main>
     <AppSectionTitle name="Team" />
-    <div class="grid grid-flow-row md:grid-cols-2 2xl:grid-cols-4 mx-auto gap-12">
+    <div
+      class="grid grid-flow-row md:grid-cols-2 2xl:grid-cols-4 mx-auto gap-12"
+    >
       <profileCard
         memberName="Axel"
         description="Musik är något Axel tycker förgyller livet. Dela gärna med dig vilken musik du gillar och om du själv spelar något instrument."
@@ -40,11 +43,24 @@ import { RouterLink } from "vue-router";
       />
       <profileCard
         memberName="Elliot"
-        description="En bra arbetsplats kan variera för Elliot, antingen ett kontor eller vid hans hund Milo."
+        description="En bra arbetsplats kan variera för Elliot, antingen kontor eller vid hans hund Milo."
         :imageName="`${elliotPicture}`"
         imageAlt="Picture of member Elliot"
         githubLink="https://github.com/elliotabrahamsson"
       />
     </div>
+
+    <section
+      class="space-y-4 mx-auto w-[150px] md:w-[450px] xl:w-[750px] 2xl:w-[1000px] max-w-full"
+    >
+      <h1 class="text-center font-bold text-4xl">Projekt</h1>
+      <ProjectCard
+        class="text-center"
+        name="Mind Dump"
+        description="Mind Dump är en webbapplikation
+    som man kan skapa anteckningar för sina behov. Den erbjuder funktioner som att
+    ta bort och redigera nuvarande anteckningar."
+      />
+    </section>
   </main>
 </template>
