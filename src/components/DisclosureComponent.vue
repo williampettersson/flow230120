b5706bb321a880a56f8bd34db44a4e4ce44d3e60
axelhagerback <axel.hagerback@gmail.com>
2023-01-12 14:26:43 +0100

    style changes to disclosure component

--- src/components/DisclosureComponent.vue
+++ src/components/DisclosureComponent.vue
@@ -13,16 +13,18 @@ export default {
 </script>
 
 <template>
-  <Disclosure v-slot="{ open }">
-    <DisclosureButton
-      class="flex w-full justify-between rounded-lg bg-slate-600 px-4 py-2 text-left text-sm font-medium hover:bg-slate-400"
-    >
-      <span>{{ title }}</span>
-      <ChevronUpIcon
-        :class="open ? 'rotate-180 transform' : ''"
-        class="h-5 w-5"
-      />
-    </DisclosureButton>
-    <DisclosurePanel>{{ description }}</DisclosurePanel>
-  </Disclosure>
+  <div class="mb-4">
+    <Disclosure v-slot="{ open }">
+      <DisclosureButton
+        class="flex w-full justify-between rounded-lg bg-slate-600 px-4 py-2 text-left text-sm font-medium hover:bg-slate-400"
+      >
+        <span>{{ title }}</span>
+        <ChevronUpIcon
+          :class="open ? 'rotate-180 transform' : ''"
+          class="h-5 w-5"
+        />
+      </DisclosureButton>
+      <DisclosurePanel class="mx-4 pt-2">{{ description }}</DisclosurePanel>
+    </Disclosure>
+  </div>
 </template>
